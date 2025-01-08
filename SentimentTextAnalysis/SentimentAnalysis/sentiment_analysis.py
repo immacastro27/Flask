5df7173fc280aefa1a52a9fd7f9758bb9e1d3c4d
immacastro27 <imma.castro27@gmail.com>
2025-01-07 19:44:53 -0600

    Sentiment Analysis from input text on web app

---
+++ SentimentTextAnalysis/SentimentAnalysis/sentiment_analysis.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def sentiment_analyzer(text_to_analyse):
+    # URL of the sentiment service
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    #Requirements for the service:
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    #Response from service:
+    response = requests.post(url, json = myobj, headers=header)
+    
+    formatted_response = json.loads(response.text)
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    # If the response status code is 500, set label and score to None
+    elif response.status_code == 500:
+        label = None
+        score = None
+    
+    dict_response = {"label": label, "score": score}
+    return dict_response
